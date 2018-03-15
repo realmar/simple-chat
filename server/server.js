17f7a3aaba61461086ffe81deb215e58a27fead0
Faruk Acibal <acfaruk@gmail.com>
2018-03-15 23:32:02 +0100

    remove double port listening

--- server/server.js
+++ server/server.js
@@ -5,10 +5,9 @@ var path = require('path');
 var express = require('express');
 
 var app = express();
-var server = require('http').Server(app);
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-app.set('port', port);
 app.use(express.static(path.join(__dirname, '/../client/')));
 server.listen(port);
 
